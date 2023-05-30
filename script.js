5fa486545533d524039bb67bc8a686de292da41e
Lucas-OliveiraDev <135034610+Lucas-OliveiraDev@users.noreply.github.com>
2023-05-30 14:19:39 -0300

    (fix) Correção Arquivo antigo

--- script.js
+++ script.js
@@ -231,3 +231,21 @@ searchInput.addEventListener("input", function () {
   currentPage = 1; // Reseta a página atual quando a pesquisa muda
   renderGlossary();
 });
+
+var animateButton = function(e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+  
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
